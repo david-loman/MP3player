2df83d554d30b7a62f20861988556d053bef8399
david-loman <linxiangpeng92@gmail.com>
2014-08-20 23:24:08 +0800

    first commit

---
+++ app/src/main/java/com/loman/david/mp3player/MainActivity.java
@@ -0,0 +1,16 @@
+package com.loman.david.mp3player;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by root on 14-8-20.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
---
+++ app/src/main/java/com/loman/david/mp3player/SplashActivity.java
@@ -0,0 +1,49 @@
+package com.loman.david.mp3player;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.os.Handler;
+
+
+public class SplashActivity extends Activity {
+
+    private final int SPLASH_DISPLAY_LENGHT=3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainIntent =new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(mainIntent);
+                SplashActivity.this.finish();
+            }
+        },SPLASH_DISPLAY_LENGHT);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.splash, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
